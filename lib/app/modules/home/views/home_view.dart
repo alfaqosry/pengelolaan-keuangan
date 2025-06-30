6ee547e93aff0c70bba5f7c71cb954d14c01f100
Alfa Qosry <alfa.qosry123@gmail.com>
2025-06-30 15:53:52 +0700

    update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,6 +11,7 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFAEC6CF),
       body: SafeArea(
         child: Column(
           children: [
@@ -18,20 +19,29 @@ class HomeView extends GetView<HomeController> {
               padding: EdgeInsetsGeometry.all(16),
               child: Row(
                 children: [
-                  CircleAvatar(backgroundColor: Colors.red),
+                  CircleAvatar(backgroundColor: Colors.white),
                   SizedBox(width: 14),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        "Halo Adam",
-                        style: TextStyle(fontSize: 14, color: Colors.grey),
-                      ),
+                      Obx(() {
+                        final user = controller.namaUser.value;
+                        final nama = user ?? 'Pengguna';
+                        return Text(
+                          "Hi,  $nama",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      }),
+
                       Text(
                         "Selamat Pagi",
                         style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Color(0xFF2F4F4F),
                         ),
                       ),
                     ],
@@ -39,9 +49,90 @@ class HomeView extends GetView<HomeController> {
                   Spacer(),
                   IconButton(
                     onPressed: () => authC.logout(),
-                    icon: Icon(Icons.logout),
+                    icon: Icon(Icons.logout, color: Colors.white),
                   ),
                 ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Pemasukan",
+                          style: TextStyle(color: Colors.green),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 16), // jarak antar card
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 5,
+                            offset: Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Pengeluaran",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(25),
+                color: Colors.grey[100],
+                child: Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Statistik",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Icon(Icons.more_horiz),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ],
